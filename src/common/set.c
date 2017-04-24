d8ab4aa5cc74bfd6591361f8c864da386d76cf19
weronika lewandowska <weronika.lewandowska@intel.com>
2017-04-24 09:52:27 +0200

    common: fix crtime set

--- src/common/set.c
+++ src/common/set.c
@@ -1651,7 +1651,14 @@ util_header_create(struct pool_set *set, unsigned repidx, unsigned partidx,
 			POOL_HDR_UUID_LEN);
 	}
 
-	hdrp->crtime = (uint64_t)time(NULL);
+	int fd_part = rep->part[partidx].fd;
+	os_stat_t stbuf;
+	if (os_fstat(fd_part, &stbuf) != 0) {
+		ERR("!fstat");
+		return -1;
+	}
+	ASSERT(stbuf.st_ctime);
+	hdrp->crtime = (uint64_t)stbuf.st_ctime;
 
 	if (!arch_flags) {
 		if (util_get_arch_flags(&hdrp->arch_flags)) {
