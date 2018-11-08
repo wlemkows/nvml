398a186ec4eb8451888fcfa6598c6097ec54de93
Weronika Lewandowska <weronika.lewandowska@intel.com>
2018-11-08 10:09:25 +0100

    tm

--- utils/docker/run-doc-update.sh
+++ utils/docker/run-doc-update.sh
@@ -61,7 +61,10 @@ git remote update
 git rebase upstream/${TARGET_BRANCH}
 
 make doc
-
+echo "----------------"
+python --version
+python3 --version
+git branch
 # Build & PR groff
 git add -A
 git commit -m "doc: automatic $TARGET_BRANCH docs update" && true
