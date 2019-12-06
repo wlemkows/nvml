a3070e8c616e4960ada971009c7c6543ba95661d
Weronika Lewandowska <weronika.lewandowska@intel.com>
2019-12-06 11:15:00 +0100

    test: tests for granularity in pmem2 (Linux part)

--- src/test/pmem2_granularity/TESTS.py
+++ src/test/pmem2_granularity/TESTS.py
@@ -34,6 +34,7 @@
 
 from enum import Enum
 import testframework as t
+import os
 
 
 class Granularity(Enum):
@@ -48,84 +49,79 @@ class PMEM2_GRANULARITY(t.BaseTest):
 
     def run(self, ctx):
         filepath = ctx.create_holey_file(16 * t.MiB, 'testfile1')
+
+        EADR_NOT_AVAILABLE = \
+            "linux_eadr_paths/eadr_not_available/sys/bus/nd/devices/"
+        EADR_AVAILABLE = \
+            "linux_eadr_paths/eadr_available/sys/bus/nd/devices/"
+
         if self.available_granularity == Granularity.BYTE:
             ctx.env['IS_EADR'] = '1'
             ctx.env['IS_PMEM'] = '1'
+            ctx.env['BUS_DEVICE_PATH'] = \
+                os.path.join(self.cwd, EADR_AVAILABLE)
         elif self.available_granularity == Granularity.CACHE_LINE:
             ctx.env['IS_EADR'] = '0'
             ctx.env['IS_PMEM'] = '1'
+            ctx.env['BUS_DEVICE_PATH'] = \
+                os.path.join(self.cwd, EADR_NOT_AVAILABLE)
         elif self.available_granularity == Granularity.PAGE:
             ctx.env['IS_EADR'] = '0'
             ctx.env['IS_PMEM'] = '0'
+            ctx.env['BUS_DEVICE_PATH'] = \
+                os.path.join(self.cwd, EADR_NOT_AVAILABLE)
+
         ctx.exec('pmem2_granularity', self.test_case, filepath)
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST0(PMEM2_GRANULARITY):
     """pass byte granularity, available byte granularity"""
     test_case = "test_granularity_req_byte_avail_byte"
     available_granularity = Granularity.BYTE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST1(PMEM2_GRANULARITY):
     """pass byte granularity, available cache line granularity"""
     test_case = "test_granularity_req_byte_avail_cl"
     available_granularity = Granularity.CACHE_LINE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST2(PMEM2_GRANULARITY):
     """pass byte granularity, available page granularity"""
     test_case = "test_granularity_req_byte_avail_page"
     available_granularity = Granularity.PAGE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST3(PMEM2_GRANULARITY):
     """pass cache line granularity, available byte granularity"""
     test_case = "test_granularity_req_cl_avail_byte"
     available_granularity = Granularity.BYTE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST4(PMEM2_GRANULARITY):
     """pass cache line granularity, available cache line granularity"""
     test_case = "test_granularity_req_cl_avail_cl"
     available_granularity = Granularity.CACHE_LINE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST5(PMEM2_GRANULARITY):
     """pass cache line granularity, available page granularity"""
     test_case = "test_granularity_req_cl_avail_page"
     available_granularity = Granularity.PAGE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST6(PMEM2_GRANULARITY):
     """pass page granularity, available byte granularity"""
     test_case = "test_granularity_req_page_avail_byte"
     available_granularity = Granularity.BYTE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST7(PMEM2_GRANULARITY):
     """pass page granularity, available cache line granularity"""
     test_case = "test_granularity_req_page_avail_cl"
     available_granularity = Granularity.CACHE_LINE
 
 
-# XXX Linux version TBD
-@t.windows_only
 class TEST8(PMEM2_GRANULARITY):
     """pass page granularity, available page granularity"""
     test_case = "test_granularity_req_page_avail_page"
