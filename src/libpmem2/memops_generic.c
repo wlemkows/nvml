pmem/pmdk

test: add pmem2_mem_ext test

src/libpmem2/memops_generic.c
@@ -132,8 +132,10 @@ void *
 memmove_nodrain_generic(void *dst, const void *src, size_t len,
 		unsigned flags, flush_func flush)
 {
+	LOG(37, "Intrinsic function: memmove_nodrain_generic");
 	LOG(15, "pmemdest %p src %p len %zu flags 0x%x", dst, src, len,
 			flags);
+
 	char *cdst = dst;
 	const char *csrc = src;
 	size_t remaining;
@@ -258,6 +260,7 @@ void *
 memset_nodrain_generic(void *dst, int c, size_t len, unsigned flags,
 		flush_func flush)
 {
+	LOG(37, "Intrinsic function: memset_nodrain_generic");
 	LOG(15, "pmemdest %p c 0x%x len %zu flags 0x%x", dst, c, len,
 			flags);
 	(void) flags;

src/libpmem2/persist.c
@@ -25,6 +25,7 @@ static void *
 memmove_nodrain_libc(void *pmemdest, const void *src, size_t len,
 		unsigned flags, flush_func flush)
 {
+	LOG(37, "Intrinsic function: memmove_nodrain_libc");
 #ifdef DEBUG
 	if (flags & ~PMEM2_F_MEM_VALID_FLAGS)
 		ERR("invalid flags 0x%x", flags);
@@ -47,6 +48,7 @@ static void *
 memset_nodrain_libc(void *pmemdest, int c, size_t len, unsigned flags,
 		flush_func flush)
 {
+	LOG(37, "Intrinsic function: memset_nodrain_libc");
 #ifdef DEBUG
 	if (flags & ~PMEM2_F_MEM_VALID_FLAGS)
 		ERR("invalid flags 0x%x", flags);

wlemkows/nvml

src/libpmem2/memops_generic.c
@@ -132,8 +132,10 @@ void *
 memmove_nodrain_generic(void *dst, const void *src, size_t len,
 		unsigned flags, flush_func flush)
 {
+	LOG(37, "Generic function: memmove_nodrain_generic");
 	LOG(15, "pmemdest %p src %p len %zu flags 0x%x", dst, src, len,
 			flags);
+
 	char *cdst = dst;
 	const char *csrc = src;
 	size_t remaining;
@@ -258,6 +260,7 @@ void *
 memset_nodrain_generic(void *dst, int c, size_t len, unsigned flags,
 		flush_func flush)
 {
+	LOG(37, "Generic function: memset_nodrain_generic");
 	LOG(15, "pmemdest %p c 0x%x len %zu flags 0x%x", dst, c, len,
 			flags);
 	(void) flags;

src/libpmem2/persist.c
@@ -25,6 +25,7 @@ static void *
 memmove_nodrain_libc(void *pmemdest, const void *src, size_t len,
 		unsigned flags, flush_func flush)
 {
+	LOG(37, "Libc function: memmove_nodrain_libc");
 #ifdef DEBUG
 	if (flags & ~PMEM2_F_MEM_VALID_FLAGS)
 		ERR("invalid flags 0x%x", flags);
@@ -47,6 +48,7 @@ static void *
 memset_nodrain_libc(void *pmemdest, int c, size_t len, unsigned flags,
 		flush_func flush)
 {
+	LOG(37, "Libc function: memset_nodrain_libc");
 #ifdef DEBUG
 	if (flags & ~PMEM2_F_MEM_VALID_FLAGS)
 		ERR("invalid flags 0x%x", flags);