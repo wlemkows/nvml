pmem/pmdk

test: add pmemset_persist

src/test/pmemset_persist/pmemset_persist.c
@@ -1,0 +1,258 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * pmemset_persist.c -- pmemset_part unittests
+ */
+
+#include <string.h>
+
+#include "config.h"
+#include "fault_injection.h"
+#include "libpmemset.h"
+#include "libpmem2.h"
+#include "out.h"
+#include "part.h"
+#include "source.h"
+#include "unittest.h"
+#include "ut_pmemset_utils.h"
+
+static void create_config(struct pmemset_config **cfg) {
+	int ret = pmemset_config_new(cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+
+	ret = pmemset_config_set_required_store_granularity(*cfg,
+		PMEM2_GRANULARITY_PAGE);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+}
+
+/*
+ * test_persist_single_part - test pmemset_persist on single part
+ */
+static int
+test_persist_single_part(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_single_part <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, 64 * 1024);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	struct pmemset_part_descriptor desc;
+	ret = pmemset_part_map(&part, NULL, &desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTeq(part, NULL);
+
+	memset(desc.addr, 1, desc.size);
+	pmemset_persist(set, desc.addr, desc.size);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_multiple_parts - test pmemset_persist on multiple
+ * parts
+ */
+static int
+test_persist_multiple_parts(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_multiple_parts <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t first_part_size = 64 * 1024;
+	size_t second_part_size = 128 * 1024;
+	struct pmemset_part_descriptor first_desc;
+	struct pmemset_part_descriptor second_desc;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, first_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &first_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, second_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &second_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(first_desc.addr, 1, first_desc.size);
+	memset(second_desc.addr, 2, second_desc.size);
+	pmemset_persist(set, first_desc.addr, first_desc.size);
+	pmemset_persist(set, second_desc.addr, second_desc.size);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_incomplete - test pmemset_persist on part of the mapping,
+ * this test should failed under pmemcheck
+ */
+static int
+test_persist_incomplete(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_incomplete <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t part_size = 64 * 1024;
+	struct pmemset_part_descriptor desc;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(desc.addr, 1, desc.size);
+	pmemset_persist(set, desc.addr, desc.size / 2);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_flush_drain - test pmemset_flush on 2 maps
+ * and drain, do not flush 1/2 of second map - should failed
+ */
+static int
+test_set_flush_drain(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_set_flush_drain <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t first_part_size = 64 * 1024;
+	size_t second_part_size = 32 * 1024;
+	struct pmemset_part_descriptor first_desc;
+	struct pmemset_part_descriptor second_desc;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, first_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &first_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, second_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &second_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(first_desc.addr, 1, first_desc.size);
+	memset(second_desc.addr, 2, second_desc.size);
+	pmemset_flush(set, first_desc.addr, first_desc.size);
+	pmemset_flush(set, second_desc.addr, second_desc.size / 2);
+	pmemset_drain(set);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_persist_single_part),
+	TEST_CASE(test_persist_multiple_parts),
+	TEST_CASE(test_persist_incomplete),
+	TEST_CASE(test_set_flush_drain),
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char **argv)
+{
+	START(argc, argv, "pmemset_persist");
+
+	util_init();
+	out_init("pmemset_persist", "TEST_LOG_LEVEL", "TEST_LOG_FILE", 0, 0);
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	out_fini();
+
+	DONE(NULL);
+}

wlemkows/nvml

src/test/pmemset_persist/pmemset_persist.c
@@ -1,0 +1,250 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * pmemset_persist.c -- pmemset_part unittests
+ */
+
+#include "out.h"
+#include "unittest.h"
+#include "ut_pmemset_utils.h"
+
+static void create_config(struct pmemset_config **cfg) {
+	int ret = pmemset_config_new(cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+
+	ret = pmemset_config_set_required_store_granularity(*cfg,
+		PMEM2_GRANULARITY_PAGE);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+}
+
+/*
+ * test_persist_single_part - test pmemset_persist on single part
+ */
+static int
+test_persist_single_part(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_single_part <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, 64 * 1024);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	struct pmemset_part_descriptor desc;
+	ret = pmemset_part_map(&part, NULL, &desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTeq(part, NULL);
+
+	memset(desc.addr, 1, desc.size);
+	pmemset_persist(set, desc.addr, desc.size);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_multiple_parts - test pmemset_persist on multiple
+ * parts
+ */
+static int
+test_persist_multiple_parts(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_multiple_parts <path1>");
+
+	const char *file1 = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t first_part_size = 64 * 1024;
+	size_t second_part_size = 128 * 1024;
+	struct pmemset_part_descriptor first_desc;
+	struct pmemset_part_descriptor second_desc;
+
+	int ret = pmemset_source_from_file(&src, file1);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, first_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &first_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, second_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &second_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(first_desc.addr, 1, first_desc.size);
+	memset(second_desc.addr, 2, second_desc.size);
+	pmemset_persist(set, first_desc.addr, first_desc.size);
+	pmemset_persist(set, second_desc.addr, second_desc.size);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_incomplete - test pmemset_persist on part of the mapping,
+ * this test should fail under pmemcheck
+ */
+static int
+test_persist_incomplete(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_persist_incomplete <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t part_size = 64 * 1024;
+	struct pmemset_part_descriptor desc;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(desc.addr, 1, desc.size);
+	pmemset_persist(set, desc.addr, desc.size / 2);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_persist_flush_drain - test pmemset_flush on 2 maps
+ * and drain, do not flush 1/2 of second map - should fail
+ */
+static int
+test_set_flush_drain(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_set_flush_drain <path>");
+
+	const char *file = argv[0];
+	struct pmemset_part *part;
+	struct pmemset_source *src;
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	size_t first_part_size = 64 * 1024;
+	size_t second_part_size = 256 * 1024;
+	struct pmemset_part_descriptor first_desc;
+	struct pmemset_part_descriptor second_desc;
+
+	int ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	create_config(&cfg);
+
+	ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, first_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &first_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_new(&part, set, src, 0, second_part_size);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_part_map(&part, NULL, &second_desc);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	memset(first_desc.addr, 1, first_desc.size);
+	memset(second_desc.addr, 2, second_desc.size);
+	pmemset_flush(set, first_desc.addr, first_desc.size);
+	pmemset_flush(set, second_desc.addr, second_desc.size / 2);
+	pmemset_drain(set);
+
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_persist_single_part),
+	TEST_CASE(test_persist_multiple_parts),
+	TEST_CASE(test_persist_incomplete),
+	TEST_CASE(test_set_flush_drain),
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char **argv)
+{
+	START(argc, argv, "pmemset_persist");
+
+	util_init();
+	out_init("pmemset_persist", "TEST_LOG_LEVEL", "TEST_LOG_FILE", 0, 0);
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	out_fini();
+
+	DONE(NULL);
+}