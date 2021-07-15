pmem/pmdk

set: merge pmemset part and add map config

src/test/pmemset_deep_flush/pmemset_deep_flush.c
@@ -18,17 +18,6 @@ FUNC_MOCK(pmem2_deep_flush, int, struct pmem2_map *map, void *ptr, size_t size)
 	}
 FUNC_MOCK_END
 
-static void create_config(struct pmemset_config **cfg) {
-	int ret = pmemset_config_new(cfg);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTne(cfg, NULL);
-
-	ret = pmemset_config_set_required_store_granularity(*cfg,
-		PMEM2_GRANULARITY_PAGE);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTne(cfg, NULL);
-}
-
 /*
  * test_deep_flush_single - test pmemset_deep_flush combinations with single map
  */
@@ -40,26 +29,25 @@ test_deep_flush_single(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_single <path>");
 
 	const char *file = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 
 	int ret = pmemset_source_from_file(&src, file);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, 64 * 1024);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ut_create_map_config(&map_cfg, set, 0, 64 * 1024, src);
+	UT_ASSERTne(map_cfg, NULL);
 
 	struct pmemset_part_descriptor desc;
-	ret = pmemset_part_map(&part, NULL, &desc);
+	ret = pmemset_map(map_cfg, NULL, &desc);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTeq(part, NULL);
 
 	/* flush single part map */
 	pmemset_deep_flush(set, desc.addr, desc.size);
@@ -88,6 +76,8 @@ test_deep_flush_single(const struct test_case *tc, int argc,
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
@@ -106,10 +96,10 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_multiple_coal <path>");
 
 	const char *file1 = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 	size_t part_size = 64 * 1024;
 	const size_t num_of_parts = 8;
 
@@ -118,7 +108,7 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 	int ret = pmemset_source_from_file(&src, file1);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
@@ -127,10 +117,11 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 			PMEMSET_COALESCING_FULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
+	ut_create_map_config(&map_cfg, set, 0, part_size, src);
+	UT_ASSERTne(map_cfg, NULL);
+
 	for (int i = 0; i < num_of_parts; i++) {
-		ret = pmemset_part_new(&part, set, src, 0, part_size);
-		UT_PMEMSET_EXPECT_RETURN(ret, 0);
-		ret = pmemset_part_map(&part, NULL, &desc);
+		ret = pmemset_map(map_cfg, NULL, &desc);
 		if (ret == PMEMSET_E_CANNOT_COALESCE_PARTS)
 			goto end;
 		UT_PMEMSET_EXPECT_RETURN(ret, 0);
@@ -194,6 +185,8 @@ end:
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
@@ -212,10 +205,10 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_multiple <path>");
 
 	const char *file1 = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 	struct pmemset_part_map *first_pmap = NULL;
 	struct pmemset_part_map *second_pmap = NULL;
 	struct pmemset_part_descriptor first_desc;
@@ -225,19 +218,18 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 	int ret = pmemset_source_from_file(&src, file1);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, part_size);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	ret = pmemset_part_map(&part, NULL, NULL);
+	ut_create_map_config(&map_cfg, set, 0, part_size, src);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map(map_cfg, NULL, NULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, part_size);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	ret = pmemset_part_map(&part, NULL, NULL);
+	ret = pmemset_map(map_cfg, NULL, NULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
 	pmemset_first_part_map(set, &first_pmap);
@@ -260,6 +252,8 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 	ret = pmemset_delete(&set);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);

src/test/pmemset_map_config/pmemset_map_config.c
@@ -1,0 +1,242 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * pmemset_map_config.c -- pmemset_map_config unittests
+ */
+
+#include <string.h>
+
+#include "config.h"
+#include "fault_injection.h"
+#include "libpmemset.h"
+#include "libpmem2.h"
+#include "out.h"
+#include "part.h"
+#include "source.h"
+#include "unittest.h"
+#include "ut_pmemset_utils.h"
+
+/*
+ * test_map_config_new_enomem - test map_config allocation with error injection
+ */
+static int
+test_map_config_new_enomem(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_map_config_new_enomem <path>");
+
+	const char *file = argv[0];
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	if (!core_fault_injection_enabled())
+		return 1;
+
+	ut_create_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(src, NULL);
+
+	core_inject_fault_at(PMEM_MALLOC, 1, "pmemset_malloc");
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, -ENOMEM);
+	UT_ASSERTeq(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_config_new_valid_source_file - create a new map_config with a source
+ *                                   with valid path assigned
+ */
+static int
+test_map_config_new_valid_source_file(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_valid_source_file <path>");
+
+	const char *file = argv[0];
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_source_from_file(&src, file);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_config_new_valid_source_pmem2 - create a new map_config with
+ *			a source with valid pmem2_source assigned
+ */
+static int
+test_map_config_new_valid_source_pmem2(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_valid_source_pmem2 <path>");
+
+	const char *file = argv[0];
+	struct pmem2_source *pmem2_src;
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	int fd = OPEN(file, O_RDWR);
+
+	ret = pmem2_source_from_fd(&pmem2_src, fd);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemset_source_from_pmem2(&src, pmem2_src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(src, NULL);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	CLOSE(fd);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_coinfig_new_invalid_source_ - create a new map_config with
+ *					an invalid source
+ */
+static int
+test_map_config_new_invalid_source(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_invalid_source");
+
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, NULL);
+	UT_PMEMSET_EXPECT_RETURN(ret, PMEMSET_E_INVALID_SOURCE_TYPE);
+
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_delete_null_config - test pmemset_map_config_delete on NULL config
+ */
+static int
+test_delete_null_config(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	struct pmemset_map_config *cfg = NULL;
+	/* should not crash */
+	int ret = pmemset_map_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTeq(cfg, NULL);
+
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_map_config_new_enomem),
+	TEST_CASE(test_map_config_new_valid_source_file),
+	TEST_CASE(test_map_config_new_valid_source_pmem2),
+	TEST_CASE(test_delete_null_config),
+	TEST_CASE(test_map_config_new_invalid_source),
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char **argv)
+{
+	START(argc, argv, "pmemset_map_config");
+
+	util_init();
+	out_init("pmemset_map_config", "TEST_LOG_LEVEL", "TEST_LOG_FILE", 0, 0);
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	out_fini();
+
+	DONE(NULL);
+}

src/test/unittest/ut_pmemset_utils.c
@@ -31,3 +31,35 @@ void ut_pmemset_expect_return(const char *file, int line, const char *func,
 				expected);
 	}
 }
+
+/*
+ * ut_create_config -- create pmemset config with default
+ * granularity value for test
+ */
+void ut_create_config(struct pmemset_config **cfg) {
+	int ret = pmemset_config_new(cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+
+	ret = pmemset_config_set_required_store_granularity(*cfg,
+		PMEM2_GRANULARITY_PAGE);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+}
+
+/*
+ * ut_create_config -- create pmemset map config using test args
+ */
+void ut_create_map_config(struct pmemset_map_config **map_cfg,
+		struct pmemset *set, size_t offset, size_t length,
+		struct pmemset_source *src) {
+	int ret = pmemset_map_config_new(map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(*map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	pmemset_map_config_set_offset(*map_cfg, offset);
+	pmemset_map_config_set_length(*map_cfg, length);
+	UT_ASSERTne(map_cfg, NULL);
+}

src/test/unittest/ut_pmemset_utils.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2021, Intel Corporation */
 
 /*
  * ut_pmemset_utils.h -- utility helper functions for libpmemset tests
@@ -17,3 +17,9 @@ void ut_pmemset_expect_return(const char *file, int line, const char *func,
 		int value, int expected);
 
 #endif /* UT_PMEMSET_UTILS_H */
+
+void ut_create_config(struct pmemset_config **cfg);
+
+void ut_create_map_config(struct pmemset_map_config **map_cfg,
+		struct pmemset *set, size_t offset, size_t length,
+		struct pmemset_source *src);

wlemkows/nvml

src/test/pmemset_deep_flush/pmemset_deep_flush.c
@@ -18,17 +18,6 @@ FUNC_MOCK(pmem2_deep_flush, int, struct pmem2_map *map, void *ptr, size_t size)
 	}
 FUNC_MOCK_END
 
-static void create_config(struct pmemset_config **cfg) {
-	int ret = pmemset_config_new(cfg);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTne(cfg, NULL);
-
-	ret = pmemset_config_set_required_store_granularity(*cfg,
-		PMEM2_GRANULARITY_PAGE);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTne(cfg, NULL);
-}
-
 /*
  * test_deep_flush_single - test pmemset_deep_flush combinations with single map
  */
@@ -40,26 +29,25 @@ test_deep_flush_single(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_single <path>");
 
 	const char *file = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 
 	int ret = pmemset_source_from_file(&src, file);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_set_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, 64 * 1024);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ut_create_map_config(&map_cfg, set, 0, 64 * 1024, src);
+	UT_ASSERTne(map_cfg, NULL);
 
 	struct pmemset_part_descriptor desc;
-	ret = pmemset_part_map(&part, NULL, &desc);
+	ret = pmemset_map(map_cfg, NULL, &desc);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	UT_ASSERTeq(part, NULL);
 
 	/* flush single part map */
 	pmemset_deep_flush(set, desc.addr, desc.size);
@@ -88,6 +76,8 @@ test_deep_flush_single(const struct test_case *tc, int argc,
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
@@ -106,10 +96,10 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_multiple_coal <path>");
 
 	const char *file1 = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 	size_t part_size = 64 * 1024;
 	const size_t num_of_parts = 8;
 
@@ -118,7 +108,7 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 	int ret = pmemset_source_from_file(&src, file1);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_set_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
@@ -127,10 +117,11 @@ test_deep_flush_multiple_coal(const struct test_case *tc, int argc,
 			PMEMSET_COALESCING_FULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
+	ut_create_map_config(&map_cfg, set, 0, part_size, src);
+	UT_ASSERTne(map_cfg, NULL);
+
 	for (int i = 0; i < num_of_parts; i++) {
-		ret = pmemset_part_new(&part, set, src, 0, part_size);
-		UT_PMEMSET_EXPECT_RETURN(ret, 0);
-		ret = pmemset_part_map(&part, NULL, &desc);
+		ret = pmemset_map(map_cfg, NULL, &desc);
 		if (ret == PMEMSET_E_CANNOT_COALESCE_PARTS)
 			goto end;
 		UT_PMEMSET_EXPECT_RETURN(ret, 0);
@@ -194,6 +185,8 @@ end:
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
@@ -212,10 +205,10 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 		UT_FATAL("usage: test_deep_flush_multiple <path>");
 
 	const char *file1 = argv[0];
-	struct pmemset_part *part;
 	struct pmemset_source *src;
 	struct pmemset *set;
 	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
 	struct pmemset_part_map *first_pmap = NULL;
 	struct pmemset_part_map *second_pmap = NULL;
 	struct pmemset_part_descriptor first_desc;
@@ -225,19 +218,18 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 	int ret = pmemset_source_from_file(&src, file1);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	create_config(&cfg);
+	ut_create_set_config(&cfg);
 
 	ret = pmemset_new(&set, cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, part_size);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	ret = pmemset_part_map(&part, NULL, NULL);
+	ut_create_map_config(&map_cfg, set, 0, part_size, src);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map(map_cfg, NULL, NULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
-	ret = pmemset_part_new(&part, set, src, 0, part_size);
-	UT_PMEMSET_EXPECT_RETURN(ret, 0);
-	ret = pmemset_part_map(&part, NULL, NULL);
+	ret = pmemset_map(map_cfg, NULL, NULL);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 
 	pmemset_first_part_map(set, &first_pmap);
@@ -260,6 +252,8 @@ test_deep_flush_multiple(const struct test_case *tc, int argc,
 	ret = pmemset_delete(&set);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);
 	ret = pmemset_source_delete(&src);
 	UT_PMEMSET_EXPECT_RETURN(ret, 0);

src/test/pmemset_map_config/pmemset_map_config.c
@@ -1,0 +1,242 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+/*
+ * pmemset_map_config.c -- pmemset_map_config unittests
+ */
+
+#include <string.h>
+
+#include "config.h"
+#include "fault_injection.h"
+#include "libpmemset.h"
+#include "libpmem2.h"
+#include "out.h"
+#include "part.h"
+#include "source.h"
+#include "unittest.h"
+#include "ut_pmemset_utils.h"
+
+/*
+ * test_map_config_new_enomem - test map_config allocation with error injection
+ */
+static int
+test_map_config_new_enomem(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL("usage: test_map_config_new_enomem <path>");
+
+	const char *file = argv[0];
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	if (!core_fault_injection_enabled())
+		return 1;
+
+	ut_create_set_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_source_from_file(&src, file);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(src, NULL);
+
+	core_inject_fault_at(PMEM_MALLOC, 1, "pmemset_malloc");
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, -ENOMEM);
+	UT_ASSERTeq(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_config_new_valid_source_file - create a new map_config with a source
+ *                                   with valid path assigned
+ */
+static int
+test_map_config_new_valid_source_file(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_valid_source_file <path>");
+
+	const char *file = argv[0];
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_set_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_source_from_file(&src, file);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_config_new_valid_source_pmem2 - create a new map_config with
+ *			a source with valid pmem2_source assigned
+ */
+static int
+test_map_config_new_valid_source_pmem2(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_valid_source_pmem2 <path>");
+
+	const char *file = argv[0];
+	struct pmem2_source *pmem2_src;
+	struct pmemset *set;
+	struct pmemset_source *src;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_set_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	int fd = OPEN(file, O_RDWR);
+
+	ret = pmem2_source_from_fd(&pmem2_src, fd);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemset_source_from_pmem2(&src, pmem2_src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(src, NULL);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_source_delete(&src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	CLOSE(fd);
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_map_coinfig_new_invalid_source_ - create a new map_config with
+ *					an invalid source
+ */
+static int
+test_map_config_new_invalid_source(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	if (argc < 1)
+		UT_FATAL(
+			"usage: test_map_config_new_invalid_source");
+
+	struct pmemset *set;
+	struct pmemset_config *cfg;
+	struct pmemset_map_config *map_cfg;
+
+	ut_create_set_config(&cfg);
+
+	int ret = pmemset_new(&set, cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	ret = pmemset_map_config_new(&map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(map_cfg, NULL);
+	UT_PMEMSET_EXPECT_RETURN(ret, PMEMSET_E_INVALID_SOURCE_TYPE);
+
+	ret = pmemset_map_config_delete(&map_cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_delete(&set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	ret = pmemset_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+
+	return 1;
+}
+
+/*
+ * test_delete_null_config - test pmemset_map_config_delete on NULL config
+ */
+static int
+test_delete_null_config(const struct test_case *tc, int argc,
+		char *argv[])
+{
+	struct pmemset_map_config *cfg = NULL;
+	/* should not crash */
+	int ret = pmemset_map_config_delete(&cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTeq(cfg, NULL);
+
+	return 1;
+}
+
+/*
+ * test_cases -- available test cases
+ */
+static struct test_case test_cases[] = {
+	TEST_CASE(test_map_config_new_enomem),
+	TEST_CASE(test_map_config_new_valid_source_file),
+	TEST_CASE(test_map_config_new_valid_source_pmem2),
+	TEST_CASE(test_delete_null_config),
+	TEST_CASE(test_map_config_new_invalid_source),
+};
+
+#define NTESTS (sizeof(test_cases) / sizeof(test_cases[0]))
+
+int
+main(int argc, char **argv)
+{
+	START(argc, argv, "pmemset_map_config");
+
+	util_init();
+	out_init("pmemset_map_config", "TEST_LOG_LEVEL", "TEST_LOG_FILE", 0, 0);
+	TEST_CASE_PROCESS(argc, argv, test_cases, NTESTS);
+	out_fini();
+
+	DONE(NULL);
+}

src/test/unittest/ut_pmemset_utils.c
@@ -31,3 +31,35 @@ void ut_pmemset_expect_return(const char *file, int line, const char *func,
 				expected);
 	}
 }
+
+/*
+ * ut_create_set_config -- create pmemset config with default
+ * granularity value for test
+ */
+void ut_create_set_config(struct pmemset_config **cfg) {
+	int ret = pmemset_config_new(cfg);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+
+	ret = pmemset_config_set_required_store_granularity(*cfg,
+		PMEM2_GRANULARITY_PAGE);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(cfg, NULL);
+}
+
+/*
+ * ut_create_map_config -- create pmemset map config using test args
+ */
+void ut_create_map_config(struct pmemset_map_config **map_cfg,
+		struct pmemset *set, size_t offset, size_t length,
+		struct pmemset_source *src) {
+	int ret = pmemset_map_config_new(map_cfg, set);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	UT_ASSERTne(map_cfg, NULL);
+
+	ret = pmemset_map_config_set_source(*map_cfg, src);
+	UT_PMEMSET_EXPECT_RETURN(ret, 0);
+	pmemset_map_config_set_offset(*map_cfg, offset);
+	pmemset_map_config_set_length(*map_cfg, length);
+	UT_ASSERTne(map_cfg, NULL);
+}

src/test/unittest/ut_pmemset_utils.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
-/* Copyright 2020, Intel Corporation */
+/* Copyright 2020-2021, Intel Corporation */
 
 /*
  * ut_pmemset_utils.h -- utility helper functions for libpmemset tests
@@ -17,3 +17,9 @@ void ut_pmemset_expect_return(const char *file, int line, const char *func,
 		int value, int expected);
 
 #endif /* UT_PMEMSET_UTILS_H */
+
+void ut_create_set_config(struct pmemset_config **cfg);
+
+void ut_create_map_config(struct pmemset_map_config **map_cfg,
+		struct pmemset *set, size_t offset, size_t length,
+		struct pmemset_source *src);